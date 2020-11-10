b808f90581d780a18cd3a30a6a410c2368925273
itsmanjeet <itsmanjeet1998@gmail.com>
2020-11-10 13:23:36 +0530

    added unit tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,41 +7,19 @@
 use rlxos::println;
 use core::panic::PanicInfo;
 
-
-/*
- * Entry Point
- */
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-
-    println!("Hello World {}", "!");
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-
-/*
- * Panic Handler
- */
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-
-
-// Test cased
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rlxos::test_panic_handler(info)
 }
 
 #[test_case]
-fn trival_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
